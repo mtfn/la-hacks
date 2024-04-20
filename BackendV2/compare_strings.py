3736d12558a7dfde17413245ff9dfda530abef0e
Fong Clement Vo <102884924+ClemHubble@users.noreply.github.com>
2024-04-20 16:47:14 -0700

    Add files via upload

---
+++ BackendV2/compare_strings.py
@@ -0,0 +1,11 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyA0WBx3-Y12rzKIwNDMlpC_mPlFDSL1xHM")
+
+comparison_model = genai.GenerativeModel('gemini-pro')
+chat = comparison_model.start_chat(history=[])
+
+def compare_strings(str1, str2):
+    response = chat.send_message("Output a percentage describing how similar String 1: " + str1 + " and String 2: " + str2 + " are to each other.")
+    print(response.text)
+    return response.text
---
+++ BackendV2/img_description.py
@@ -0,0 +1,19 @@
+import google.generativeai as genai
+import PIL.Image
+
+genai.configure(api_key="AIzaSyA0WBx3-Y12rzKIwNDMlpC_mPlFDSL1xHM")
+
+model = genai.GenerativeModel('gemini-pro-vision')
+
+prompt = """Given this image, describe what you see in this image. Separate objects that you see by commas. Return output as a String: "object1, object2, object3, etc"""
+
+def get_image(str):
+    return PIL.Image.open(str)
+
+def generate_string_from_image(img):
+    response = model.generate_content([prompt, img])
+    print(response.text)
+    return response.text
+
+#Test Code
+generate_string_from_image(get_image("IMG_0036.jpg"))
