d4cfe3005f01557b11eb7bc142077e2c9ac389eb
Fong Clement Vo <102884924+ClemHubble@users.noreply.github.com>
2024-04-20 22:13:50 -0700

    Update compare_strings.py

--- BackendV2/compare_strings.py
+++ BackendV2/compare_strings.py
@@ -7,7 +7,7 @@ comparison_model = genai.GenerativeModel('gemini-pro')
 chat = comparison_model.start_chat(history=[])
 
 def compare_strings(str1, str2):
-    response = chat.send_message(f"Output a percentage describing how similar String 1: \"{str1}\" and String 2: \"{str2}\" are to each other.")
+    response = chat.send_message("Output on a scale of 1 to 5 describing how similar in meaning String 1: " + str1 + " and String 2: " + str2 + " are to each other, with 5 being exact strings and 1 being no matching similar words between the 2 strings. If you can't decide on a number, just pick a random number between 1 to 5 inclusive. Please do not spell out the number, output an integer instead.")
     print(response.text)
     return response.text
 
