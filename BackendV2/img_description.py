3eff28fd622282dc3c932276b2612f8c646ac60e
aryaman-dayal <151600746+aryaman-dayal@users.noreply.github.com>
2024-04-20 21:49:40 -0700

    Update img_description.py

--- BackendV2/img_description.py
+++ BackendV2/img_description.py
@@ -1,3 +1,4 @@
+import sys
 import google.generativeai as genai
 import PIL.Image
 
@@ -5,15 +6,16 @@ genai.configure(api_key="AIzaSyA0WBx3-Y12rzKIwNDMlpC_mPlFDSL1xHM")
 
 model = genai.GenerativeModel('gemini-pro-vision')
 
-prompt = """Given this image, describe what you see in this image. Separate objects that you see by commas. Return output as a String: "object1, object2, object3, etc"""
+prompt = "Given this image, describe what you see in this image. Separate objects that you see by commas. Return output as a String: \"object1, object2, object3, etc\""
 
-def get_image(str):
-    return PIL.Image.open(str)
+def get_image(image_path):
+    return PIL.Image.open(image_path)
 
-def generate_string_from_image(img):
+def generate_string_from_image(image_path):
+    img = get_image(image_path)
     response = model.generate_content([prompt, img])
     print(response.text)
-    return response.text
 
-#Test Code
-generate_string_from_image(get_image("IMG_0036.jpg"))
+
+image_path = sys.argv[1]  # Takes the image path from command-line arguments
+generate_string_from_image(image_path)
